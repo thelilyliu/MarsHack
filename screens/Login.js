864cc192ea656727dab74e188433f1957d11da09
Lily Liu <thelilyliu@gmail.com>
2018-09-22 00:40:22 -0400

    Navigate between screens

--- screens/Login.js
+++ screens/Login.js
@@ -13,16 +13,32 @@ export default class LoginScreen extends Component {
     return (
       <Container>
         <Content style={{ padding: 15 }}>
-          <Button block style={{ marginBottom: 15 }}>
+          <Button
+            block
+            style={{ marginBottom: 15 }}
+            onPress={() => this.props.navigation.push('Home')}
+          >
             <Text>Lily Liu</Text>
           </Button>
-          <Button block style={{ marginBottom: 15 }}>
+          <Button
+            block
+            style={{ marginBottom: 15 }}
+            onPress={() => this.props.navigation.push('Home')}
+          >
             <Text>Ben Xiao</Text>
           </Button>
-          <Button block style={{ marginBottom: 15 }}>
+          <Button
+            block
+            style={{ marginBottom: 15 }}
+            onPress={() => this.props.navigation.push('Home')}
+          >
             <Text>Kevin Trieu</Text>
           </Button>
-          <Button block style={{ marginBottom: 15 }}>
+          <Button
+            block
+            style={{ marginBottom: 15 }}
+            onPress={() => this.props.navigation.push('Home')}
+          >
             <Text>Haibo Wang</Text>
           </Button>
         </Content>
